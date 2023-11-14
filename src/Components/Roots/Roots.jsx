964e205fce403700ff80e00f570f1c8aecfe8a75
AshikurZaman01 <ashikurzaman774@gmail.com>
2023-11-14 11:54:18 +0600

    add banner

--- src/Components/Roots/Roots.jsx
+++ src/Components/Roots/Roots.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../Pages/Shared/Footer/Footer';
+import Navbar from '../Pages/Home/Navbar';
 
 const Roots = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
